0d70d0f417d28f203168de4aca61cd68e06d6275
Alex Jensen <alex@endure.io>
2019-12-10 22:08:05 -0500

    fixed setup script

--- Setup/Setup.sh
+++ Setup/Setup.sh
@@ -3,3 +3,4 @@ echo "alias ArgumentScript='sh $PWD/ArgumentScript.sh \$1'" >> $HOME/.bashrc
 echo "java -jar $PWD/ArgumentScript.jar \$1" >> $PWD/ArgumentScript.sh
 echo "javac Out.java" >> $PWD/ArgumentScript.sh
 echo "java Out" >> $PWD/ArgumentScript.sh
+source ~/.bashrc
